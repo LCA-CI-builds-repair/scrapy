fb8aaef842d87397dd0e6dda0876f5557ae422d7
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-16 13:45:55 +0000

    waleko__claude-3.5-sonnet_c81f__id_20

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -179,9 +179,9 @@ class Crawler:
             yield maybeDeferred(self.engine.stop)
 
     @staticmethod
-    def _get_component(cls, components):
+    def _get_component(target_cls, components):
         for component in components:
-            if type(component) is cls:
+            if isinstance(component, target_cls):
                 return component
         return None
 
